7cd83e33e64c14807b46546420df484404ce7316
Viktror-INC <106181035+008vtg@users.noreply.github.com>
2023-02-05 18:14:45 +0200

    fix: correct data, added new logic for showing constants

---
+++ src/static-data/content/variables.ts
@@ -0,0 +1,6 @@
+export const domain = "vestatg.com";
+export const newDomain = "vex-t.com";
+export const phoneNumber = "+74950452241";
+export const email = "777vestg@gmail.com";
+export const fullProjectName = "vExnes";
+export const shortProjectName = "VEX";

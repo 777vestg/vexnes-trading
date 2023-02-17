6bc272135544d97ba64c87c5344c0cd1870aa139
Viktror-INC <106181035+008vtg@users.noreply.github.com>
2023-02-17 16:07:43 +0200

    change phone number

--- src/static-data/content/variables.ts
+++ src/static-data/content/variables.ts
@@ -1,5 +1,5 @@
 export const domain = "vex-t.com";
-export const phoneNumber = "+74950452241";
+export const phoneNumber = "+74951514017";
 export const email = "support@vex-t.com";
 export const fullProjectName = "vExnes";
 export const shortProjectName = "VEX";

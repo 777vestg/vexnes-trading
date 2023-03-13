e29bf18e28170d7bc1dba0dd679c514cd1202e5d
008vtg <106181035+008vtg@users.noreply.github.com>
2023-03-13 16:41:06 +0200

    change domain

--- src/static-data/content/variables.ts
+++ src/static-data/content/variables.ts
@@ -1,4 +1,4 @@
-export const domain = "vex-t.com";
+export const domain = "vexnes.com";
 export const phoneNumber = "+74951514017";
 export const email = "support@vex-t.com";
 export const fullProjectName = "vExnes";
